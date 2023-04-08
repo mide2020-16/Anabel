c0c2e12059095e759822fb4bbd492a8e585ed3d7
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-04-08 17:52:28 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const playBtn = document.querySelector(".play-button"),
+  brush = document.querySelector(".brush"), 
+  container = document.querySelector(".container")
+
+playBtn.addEventListener("click", function () {
+  brush.style.opacity = 1  
+})
