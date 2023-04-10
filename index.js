e16624effbcc6ab73d588ff2155bd4163ed32f0f
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-04-10 23:52:07 +0100

    created new ui for intro

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const playBtn = document.querySelector(".play-button"),
   brush = document.querySelector(".brush"), 
-  container = document.querySelector(".container")
+  container = document.querySelector(".container"),
+  animationPage = document.querySelector(".intro__anime"),
+  nextPage = document.querySelector(".nextpage")
 
 playBtn.addEventListener("click", function () {
   brush.style.opacity = 1  
+})
+nextPage.addEventListener("click", function (e) {
+  e.preventDefault()
+  animationPage.style.display = "none"
+  container.classList.add("fade-in")
 })
