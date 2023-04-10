4af8a450548d80a543a88d330430977bb49208f1
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-04-11 00:47:18 +0100

    proper formatting of files

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const playBtn = document.querySelector(".play-button"),
-  brush = document.querySelector(".brush"), 
+  brush = document.querySelector(".brush"),
   container = document.querySelector(".container"),
   animationPage = document.querySelector(".intro__anime"),
-  nextPage = document.querySelector(".nextpage")
+  nextPage = document.querySelector(".nextpage");
 
 playBtn.addEventListener("click", function () {
-  brush.style.opacity = 1  
-})
+  brush.style.opacity = 1;
+});
 nextPage.addEventListener("click", function (e) {
-  e.preventDefault()
-  animationPage.style.display = "none"
-  container.classList.add("fade-in")
-})
+  e.preventDefault();
+  animationPage.style.display = "none";
+  container.classList.add("fade-in");
+});
